73579c14a832a2b1358451859897c264fc34060b
Torgeir Beyer <torgeir.beyer@gmail.com>
2018-01-16 18:11:54 +0100

    progress: with stream and pipe

--- app.js
+++ app.js
@@ -2,14 +2,21 @@
 
 // get the http module from node.js
 const http = require('http');
+const fs = require('fs');
 
 // createServer is a built-in method in node
 http.createServer(function(req, res) {
 
   // first we create the headersin the response
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  //send the response aas a stream with a pipe
+  let html = fs.createReadStream(__dirname + '/index.html').pipe(res);
+  
+  
+  // const message = 'Helloooo WORLD!';
+  // html = html.replace('{Message}', message);
   //this means the response is ending and its the last thing in the res
-  res.end('Hello world\n');
+  // res.end(html);
 
 //Listen on a port(number) and an adress(string). Port can be anything, the IP now is localhost
 }).listen(1337, '127.0.0.1') 
